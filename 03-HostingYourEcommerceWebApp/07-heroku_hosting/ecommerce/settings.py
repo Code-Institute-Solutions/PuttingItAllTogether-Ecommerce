ea21106485350dba86713b4ef15ee6d3e7fa2b1d
Nakita McCool <nakitamccool@gmail.com>
2019-04-16 10:22:23 +0100

    Updated MEDIA_URL to point to S3

--- 03-HostingYourEcommerceWebApp/07-heroku_hosting/ecommerce/settings.py
+++ 03-HostingYourEcommerceWebApp/07-heroku_hosting/ecommerce/settings.py
@@ -161,7 +161,7 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
