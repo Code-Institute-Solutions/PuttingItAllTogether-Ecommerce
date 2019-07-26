7dbb5bb026984f47dbf434b0e275cfa2e8b5d863
NielMc <nielmce@gmail.com>
2019-07-26 15:13:09 +0100

    Update views.py

--- 02-FindingAndPurchasingProducts/01-searching_for_a_product/search/views.py
+++ 02-FindingAndPurchasingProducts/01-searching_for_a_product/search/views.py
@@ -3,5 +3,5 @@ from products.models import Product
 
 # Create your views here.
 def do_search(request):
-    products = Products.objects.filter(name__icontains=request.GET['q'])
+    products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
